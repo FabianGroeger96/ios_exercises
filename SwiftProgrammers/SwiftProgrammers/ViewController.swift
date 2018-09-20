16427e389e9d6fa4c56633d6815a27d442aeb290
Fabian Gröger <fabian.groeger@bluewin.ch>
2018-09-20 16:50:08 +0200

    init

---
+++ SwiftProgrammers/SwiftProgrammers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftProgrammers
+//
+//  Created by Fabian Gröger on 20.09.18.
+//  Copyright © 2018 Fabian Gröger HSLU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
